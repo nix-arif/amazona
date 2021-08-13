106838ea863d4bcd78a614ea4b88741dc4594820
Vision Schaft Resources <visionschaftresources@Visions-MacBook-Pro.local>
2021-08-13 15:52:17 +0800

    Setup mongoose, mongodb database, creating model, handle error db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost/amazona",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("Connection Success");
+  }
+);
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
@@ -18,6 +27,16 @@ app.get("/api/products/:id", (req, res) => {
   } else {
     res.status(404).send({ message: "Product not Found" });
   }
+});
+
+app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
