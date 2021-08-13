08612f7f830407cbfc4cc01c17bba22e8e6be9b1
Vision Schaft Resources <visionschaftresources@Visions-MacBook-Pro.local>
2021-08-13 16:23:02 +0800

    setting product model, insert in db, implement backend for product

--- backend/data.js
+++ backend/data.js
@@ -17,7 +17,6 @@ const data = {
   ],
   products: [
     {
-      _id: "1",
       name: "Nike Slim Shirt",
       category: "Shirts",
       image: "/images/p1.jpeg",
@@ -29,7 +28,6 @@ const data = {
       description: "hign quality product",
     },
     {
-      _id: "2",
       name: "Adidas Fit Shirt",
       category: "Shirts",
       image: "/images/p2.jpeg",
@@ -41,7 +39,6 @@ const data = {
       description: "hign quality product",
     },
     {
-      _id: "3",
       name: "Lacoste Free Shirt",
       category: "Shirts",
       image: "/images/p3.jpeg",
@@ -53,7 +50,6 @@ const data = {
       description: "hign quality product",
     },
     {
-      _id: "4",
       name: "Nike Slim Pant",
       category: "Pants",
       image: "/images/p4.jpeg",
@@ -65,7 +61,6 @@ const data = {
       description: "hign quality product",
     },
     {
-      _id: "5",
       name: "Puma Slim Pant",
       category: "Pants",
       image: "/images/p5.jpeg",
@@ -77,7 +72,6 @@ const data = {
       description: "hign quality product",
     },
     {
-      _id: "6",
       name: "Adidas Fit Pant",
       category: "Pants",
       image: "/images/p6.jpeg",
