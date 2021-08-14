4482bd5efcce55310634dfd5a4f5f7d0f05099a1
Vision Schaft Resources <visionschaftresources@Visions-MacBook-Pro.local>
2021-08-14 10:12:42 +0800

    Implement backend sign with jwtwebtoken

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/amazona",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  },
-  () => {
-    console.log("Connection Success");
-  }
-);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
